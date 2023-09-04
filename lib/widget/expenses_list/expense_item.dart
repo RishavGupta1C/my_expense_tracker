9d246fb1cf0df294da95cfc97fbff60fa11145f7
RishavGupta1C <91869768+RishavGupta1C@users.noreply.github.com>
2023-09-05 00:14:13 +0530

    commit 5

---
+++ lib/widget/expenses_list/expense_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:my_expense_tracker/models/expense.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem({Key? key}) : super(key: key);
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return
+  }
+}
