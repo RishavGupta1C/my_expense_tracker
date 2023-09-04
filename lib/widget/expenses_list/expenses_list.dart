a09183f668c44b4aa7e6c602747e7a6705f7452c
RishavGupta1C <91869768+RishavGupta1C@users.noreply.github.com>
2023-09-05 00:12:02 +0530

    commit 5

--- lib/widget/expenses_list/expenses_list.dart
+++ lib/widget/expenses_list/expenses_list.dart
@@ -2,14 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:my_expense_tracker/models/expense.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({Key? key, required this.expenses}) : super(key: key);
+  const ExpensesList({
+    Key? key,
+    required this.expenses,
+  }) : super(key: key);
 
   final List<Expense> expenses;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemBuilder: (ctx, index) => Text(),
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => Text(expenses[index].title),
     );
   }
 }
